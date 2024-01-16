fdffc2dbee5f674402ee6048a0c3e0bc64b921d1
Maianne Thornton <maianne.thornton@gmail.com>
2024-01-16 13:29:23 -0500

    build: playSong functionality for playing songs

--- script.js
+++ script.js
@@ -86,24 +86,52 @@ let userData = {
     songCurrentTime: 0
 };
 
+//implements the functionality for playing the displayed songs.
+const playSong = (id) => {
+    //iterates through the userData?.songs array, searching for a song that corresponds to the id passed into the playSong function using the find() method.
+    const song = userData?.songs.find((song) => song.id === id);
+    // tells the audio element where to find the audio data for the selected song.
+    audio.src = song.src;
+    //tells the audio element what to display as the title of the song.
+    audio.title = song.title;
+    //if statement to check whether the userData?.currentSong is null or if userData?.currentSong.id is strictly not equal song.id. If condition is true then the song starts playing from the beginning
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0;
+    } else {
+        // else play from current user position.
+        audio.currentTime = userData.songCurrentTime;
+    }
+    //update the current song being played.
+    userData.currentSong = song;
+    playButton.classList.add("playing");
+    //play() is a method from the web audio API for playing an mp3 file.
+    audio.play();
+};
+
 //function to display the songs in the UI
 const renderSongs = (array) => {
     //loop through array and build HTML for all the songs using the map() method to iterate through an array and return a new array.
-    const songsHTML = array.map((song) => {
-        return `<li id ="song-${song.id}" class ="playlist-song">
-        <button class = "playlist-song-info">
-        <span class="playlist-song-title">${song.title}</span>
-        <span class = "playlist-song-artist">${song.artist}</span>
-        <span class = "playlist-song-duration">${song.duration}</span>
+    const songsHTML = array
+        .map((song) => {
+            return `
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
         </button>
-        <button class = "playlist-song-delete" aria-label = "Delete ${song.title}">
-        <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
-        </button>
-        </li>`;
-        // Use the join() method to combine the songsHTML markup into a single string.
-    }).join("");
+      </li>
+      `;
+            // Use the join() method to combine the songsHTML markup into a single string.
+        })
+        .join("");
     //update the playlist in your HTML document to display the songs.
+
     playlistSongs.innerHTML = songsHTML;
-    //Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined.
-    renderSongs(userData?.songs);
 };
+//Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined.
+renderSongs(userData?.songs);
